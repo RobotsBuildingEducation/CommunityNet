bf5c7865940965185930b9dac5d6e2ca17723a97
RobotsBuildingEducation <65219666+RobotsBuildingEducation@users.noreply.github.com>
2025-06-09 23:06:34 -0600

    add auth page and fix lint

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,5 @@ import { createRoot } from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
 
-// FIXME: a custom font should be used. Eg:
-// import '@fontsource-variable/<font-name>';
 
 createRoot(document.getElementById("root")!).render(<App />);
